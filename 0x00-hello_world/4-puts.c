8b47d4bd7351144e446438c736c276a56b89fd1b
Fatma Yousef <fatma501yousef5@gmail.com>
2023-07-13 16:12:30 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/***
+ * main function
+ *
+ * print sentence
+ *
+ * return (success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
